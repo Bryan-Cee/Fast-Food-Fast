3b8d0979ff38e3be96c666465a5a0fb0f47670e2
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-13 09:11:48 +0300

    [ Feature #160089190 ] Add an event listener
    This commit adds an event listener for the click 
    event from accept buttons.
    
    When the admin accepts an order by clicking 
    this button the complete order button appears.

--- UI/scripts/js/main.js
+++ UI/scripts/js/main.js
@@ -1,6 +1,12 @@
- function accepted(e) {
-     e.style.display = "none";
-     e.nextElementSibling.style.display = "none";
-     var parent = e.parentNode;
-     parent.lastElementChild.style.display = "inline-block"
- }
+button = document.querySelectorAll('#accept')
+
+button.forEach(element => {
+    element.addEventListener('click', accept)
+});
+
+function accept() {
+    this.style.display = "none";
+    this.nextElementSibling.style.display = "none";
+    var parent = this.parentNode;
+    parent.lastElementChild.style.display = "inline-block"
+}
